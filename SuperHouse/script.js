84a2f8c00781131d2029a001dea7abd85b7684e8
Andeacardio <107910228+Andeacardio@users.noreply.github.com>
2023-06-16 23:18:11 +0300

    Add files via upload

---
+++ SuperHouse/script.js
@@ -0,0 +1,45 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+function typeEffect(element, speed) {
+  var text = element.innerHTML;
+  element.innerHTML = "";
+
+  var i = 0;
+  var timer = setInterval(function () {
+    if (i < text.length) {
+      element.append(text.charAt(i));
+      i++;
+    } else {
+      clearInterval(timer);
+    }
+  }, speed);
+}
+
+// application
+var speed = 75;
+var h1 = document.querySelector("h1");
+
+var delay = h1.innerHTML.length * speed + speed;
+
+// type affect to header
+typeEffect(h1, speed);
